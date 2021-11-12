3e5095b0af79c40a2471fce22e88103cca6a4147
Joni Laitinen <joni.laitinen@gmail.com>
2021-11-12 13:52:58 +0200

    Exercise 2 done plus credits

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,15 +3,61 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
+// This is custom hook done in credit 3
+// General function which could be by any app
+function useLocalStorageState(
+  key,
+  defaultValue = '',
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+
+  // CREDIT 1, change useState to call function, this way we are reading from local storage only once - instead of reading every rendering
+  // BASICALLY:
+  // function readNameFromLocalStorage() {
+  //   return window.localStorage.getItem('name') || initialName
+  // }
+  // const [name, setName] = React.useState(readNameFromLocalStorage)
+  // BUT CAN BE SIMPLIFIED:
+  const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+      // the try/catch is here in case the localStorage value was set before
+      // we had the serialization in place (like we do in previous extra credits)
+      try {
+        return deserialize(valueInLocalStorage)
+      } catch (error) {
+        window.localStorage.removeItem(key)
+      }
+    }
+    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
+  })
+
+  const prevKeyRef = React.useRef(key)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
+  // CREDIT 2: add dependencies after function like [name], will make this happen only if name changes
+  // comparing as you were using ===
+  React.useEffect(() => {
+    // your side-effect code here.
+    // this is where you can make HTTP requests or interact with browser APIs.
+    const prevKey = prevKeyRef.current
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+    }
+    prevKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
 
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+  // const [name, setName] = React.useState('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
